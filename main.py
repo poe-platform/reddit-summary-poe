f1b5363e2198c3c3369f91cea4d1bd690c706982
Anmol Singh <asingh@quora.com>
2023-07-12 21:39:11 -0700

    rotate api key and store it in secret

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from modal import Image, Secret, Stub, asgi_app
 from reddit_summary_bot import RedditSummaryBot
 
 image = Image.debian_slim().pip_install_from_requirements("requirements.txt")
-stub = Stub("reddit-summary-bot")
+stub = Stub("reddit-summary-app")
 
 
-@stub.function(image=image, secret=Secret.from_name("reddit-summary-app"))
+@stub.function(image=image, secret=Secret.from_name("reddit-summary-secret"))
 @asgi_app()
 def fastapi_app():
     bot = RedditSummaryBot(
         reddit_client_id=os.environ["REDDIT_CLIENT_ID"],
         reddit_client_secret=os.environ["REDDIT_CLIENT_SECRET"],
     )
-    POE_API_KEY = "REDACTED_POE_KEY"
+    POE_API_KEY = os.environ["POE_API_KEY"]
     app = make_app(bot, api_key=POE_API_KEY)
     return app
